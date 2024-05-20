d11069fd7b7145954b2081f73b89b4af7c92e6d7
MSTarot <167876712+mstarot@users.noreply.github.com>
2024-05-20 18:56:07 -0400

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,5 @@
+
+
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -6,3 +8,12 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+function myFunction () {
+  var x = document.getElementById("myNavbar");
+  if (x.className==="topnav") {
+    x.className += " responsive";
+  } else {
+    x.className="topnav";
+  }
+}
